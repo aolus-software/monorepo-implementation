4b4f31550ddc55d3c29015557789c6af533c0c28
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2026-01-23 19:16:00 +0700

    wip: elysia

--- apps/api-admin/src/index.ts
+++ apps/api-admin/src/index.ts
@@ -1,8 +1,9 @@
-import { Elysia } from "elysia";
 import { apiEnvSchema } from "@repo/env";
+import Elysia from "elysia";
 
 const env = apiEnvSchema.parse(process.env);
-
 const app = new Elysia().listen(env.PORT);
 
 console.log(`🦊 Admin API running at http://localhost:${env.PORT}`);
+
+export default app.fetch;
